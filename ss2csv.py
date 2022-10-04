91d7ba10f4323faac211106840e62cba5d60211f
damoncrockett <damoncrockett@hotmail.com>
2022-10-04 16:11:59 -0400

    stitched row handling

--- ss2csv.py
+++ ss2csv.py
@@ -7,49 +7,55 @@ def file2table(f):
     with open(f,'r') as colorfile:
         l = colorfile.readlines()
 
-    hunt = False
-    collist = []
-    for item in l:
-        if hunt==True:
-            if 'END_DATA_FORMAT' in item:
-                break
-            else:
-                collist.append(item)
-
+    # all column strings are identical, so we just grab the first
+    for i,item in enumerate(l):
         if 'BEGIN_DATA_FORMAT' in item:
-            hunt = True
+            colstring = l[i+1]
+            break
 
-    cols = collist[0].split('\t')
-    cols = [item.rstrip('\n') for item in cols]
+    cols = [item.rstrip('\n') for item in colstring.split('\t')]
+    ncols = len(cols)
 
+    # we hunt for data between the 'BEGIN' and 'END' flags
     hunt = False
     vallists = []
     for item in l:
         if hunt==True:
-            if item=='END_DATA\n':
+            if 'END_DATA\n' in item:
                 vallists.append(vallist)
                 hunt = False
             else:
                 vallist.append(item)
 
-        if item=='BEGIN_DATA\n':
+        if 'BEGIN_DATA\n' in item:
             hunt = True
             vallist = []
 
-    rowlist = []
-    for i,vallist in enumerate(vallists):
-            l = [item.rstrip('\n').split('\t') for item in vallist]
-            l = [item for sublist in l for item in sublist]
+    """
+    Sometimes, data from consecutive measurements will not be separated by
+    the usual 'BEGIN_DATA' and 'END_DATA' flags. In such cases, the 'hunt' above
+    will generate some data rows that are actually multiple rows stitched
+    together. So, we look for those rows, and unstitch them.
+    """
 
-            n = len([item for item in l if item!=''])
-            if n==78:
-                rowlist.append([item for item in l if item!=''])
-            else:
-                print('bad row',n)
+    rowlist = []
+    for vallist in vallists:
+        l = [item.rstrip('\n').split('\t') for item in vallist]
+        l = [item for sublist in l for item in sublist] # flatten list of lists
+        l = [item for item in l if item!='']
+        n = len(l)
+
+        if n == ncols: # the usual case
+            rowlist.append(l)
+        elif n % ncols == 0: # the stitched-together case
+            for row in [l[i:i+ncols] for i in range(0,n,ncols)]:
+                rowlist.append(row)
+        else:
+            print(os.path.basename(f),"row length {} not a multiple of ncols".format(n))
 
     sf = pd.DataFrame(rowlist,columns=cols)
 
-    return sf,lens
+    return sf
 
 def cleancols(sf):
     assert len(sf['SPECTRAL_NM'].columns) == len(sf['SPECTRAL_VAL'].columns)
